fb37b863c2668417c31f8eb2f62d9b07d62d74c2
Olga2992930 <olga2992930@gmail.com>
2025-10-02 20:58:59 +0200

    init

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView } from 'react-native-web';
+import { use, useState } from 'react';
+
+function Nav() {
+ const [activePage, setActivePage] = useState("home")
+  return (
+    <View>
+      <Button title={"Home"} onPress= {() => setActivePage("home")} />
+      <Button title={"Favoriter"} onPress= {() => setActivePage("favoriter")} />
+      <Button title={"Inställningar"} onPress= {() => setActivePage("inställningar")} />
+        
+    </View>
+  )
+}
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Nav />
+      <Text style={styles.text}>Hej och vällkommen till min väderapp!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +33,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "blue"
+  }
+
 });
